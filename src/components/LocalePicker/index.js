183415038993bcc3307d7cac86920d184ed2308e
HAMZA SARI <hamza.sari@gmail.com>
2021-09-04 18:58:00 +0300

    LocalePicker component is refactored.

--- src/components/LocalePicker/index.js
+++ src/components/LocalePicker/index.js
@@ -2,28 +2,46 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Translate } from 'react-redux-i18n';
 import PropTypes from 'prop-types';
+
 import { supportedLocales } from '../../config/i18n';
 import { setLocaleWithFallback } from '../../redux/actions/i18n';
 import './style.sass';
 
-const LocalePicker = (props) => {
+/**
+ * LocalePicker component
+ * You can see the supported locales and change current locale with this component
+ * @param setLocaleWithFallback - Function to dispatch locale state variable in redux
+ * @param locale - Current locale information in redux state
+ * @param align - Parameter to move locale picker right or left within a container
+ * @returns {JSX.Element}
+ */
+const LocalePicker = ({ setLocaleWithFallback, locale, align }) => {
+  /**
+   * Changes current locale information that is stored in redux state
+   * @param code - can be one of the keys of supportedLocales object in /src/config/i18.js
+   */
   const changeLocale = (code) => {
-    props.setLocaleWithFallback(code);
+    setLocaleWithFallback(code);
   };
 
+  /**
+   * Locales inner component
+   * This component is used to show all supported locales and when one of them is clicked, it triggers the changeLocale function with the locale code of clicked item
+   * @returns {JSX.Element}
+   */
   const Locales = () => {
     return (
       <>
-        {Object.entries(supportedLocales).map((locale) => (
+        {Object.entries(supportedLocales).map((localeItem) => (
           <button
-            key={locale[0]}
+            key={localeItem[0]}
             className={`link-button navbar-item${
-              locale[0] === props.locale ? ` is-active` : ``
+              localeItem[0] === locale ? ` is-active` : ``
             }`}
             onClick={() => {
-              changeLocale(locale[0]);
+              changeLocale(localeItem[0]);
             }}>
-            {locale[1]}
+            {localeItem[1]}
           </button>
         ))}
       </>
@@ -33,7 +51,7 @@ const LocalePicker = (props) => {
   return (
     <div
       className={`navbar-item has-dropdown is-hoverable${
-        props.align === 'right' ? ' align-right' : ''
+        align === 'right' ? ' align-right' : ''
       }`}>
       <button className={`navbar-link link-button`}>
         <span className="icon-text">
@@ -53,13 +71,46 @@ const LocalePicker = (props) => {
   );
 };
 
+/**
+ * Property types of LocalePicker component
+ * @type {{setLocaleWithFallback: Validator<NonNullable<(...args: any[]) => any>>, locale: Validator<NonNullable<string>>, align: Requireable<string>}}
+ */
 LocalePicker.propTypes = {
+  /**
+   * Current locale information in redux state
+   */
   locale: PropTypes.string.isRequired,
+  /**
+   * Function to dispatch locale state variable in redux
+   */
   setLocaleWithFallback: PropTypes.func.isRequired,
+  /**
+   * Parameter to move locale picker right or left within a container
+   */
   align: PropTypes.string,
 };
 
-const mapStateToProps = (state) => ({ locale: state.i18n.locale });
-const mapDispatchToProps = { setLocaleWithFallback };
+/**
+ * mapStateToProps function to use with redux
+ * @param state
+ * @returns {{locale}}
+ */
+const mapStateToProps = (state) => ({
+  /**
+   * Current locale information in redux state
+   */
+  locale: state.i18n.locale,
+});
+
+/**
+ * mapDispatchToProps object to use with redux
+ * @type {{setLocaleWithFallback: ((function(*=): function(*): *)|*)}}
+ */
+const mapDispatchToProps = {
+  /**
+   * Function to dispatch locale state variable in redux
+   */
+  setLocaleWithFallback,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(LocalePicker);
